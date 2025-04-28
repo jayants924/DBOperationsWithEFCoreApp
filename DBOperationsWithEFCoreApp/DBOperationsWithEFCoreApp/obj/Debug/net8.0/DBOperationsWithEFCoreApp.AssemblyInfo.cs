b6238ab9fa948ffb420a383c1da7066deda10d8b
jayants924 <jayant@shorthills.ai>
2025-04-28 16:39:46 +0530

    Add EF Core dependencies and configure DbContext

--- DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/obj/Debug/net8.0/DBOperationsWithEFCoreApp.AssemblyInfo.cs
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/obj/Debug/net8.0/DBOperationsWithEFCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBOperationsWithEFCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72fa346b209f9308b8cebd9ad0739b1539f6890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d5998578da0d1439bd8a42eab3253ce4579fad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBOperationsWithEFCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBOperationsWithEFCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
